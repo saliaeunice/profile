9595db0bd2fa17c6db2df7e900e8bb8d80cab3dd
saliaeunice <101419339+saliaeunice@users.noreply.github.com>
2022-03-16 09:55:24 -0700

    Profiling

---
+++ src/writers.js
@@ -0,0 +1,39 @@
+const writers=[  
+    {
+        id:1,
+        name: "Jamila Lorde",
+        avatar:"audre_lorde",
+        email: "jamila23@gmail.com",
+        phone: "0546282732"
+    },
+    {
+        id:2,
+        name: "Mubarik Ibrahim",
+        avatar:"belle_hooks",
+        email: "jamila23@gmail.com",
+        phone: "0546282732"
+    },
+    {
+        id:3,
+        name: "Jamila Lorde",
+        avatar:"du_bois",
+        email: "jamila23@gmail.com",
+        phone: "0546282732"
+    },
+    {
+        id:4,
+        name: "Jamila Lorde",
+        avatar:"james_baldwin",
+        email: "jamila23@gmail.com",
+        phone: "0546282732"
+    },
+    {
+        id:5,
+        name: "Jamila Lorde",
+        avatar:"maya_angelou",
+        email: "jamila23@gmail.com",
+        phone: "0546282732"
+    }
+]
+
+export default writers;
